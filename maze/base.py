21799d354ee6f16c3bcc05e4d77dbd8e36e67e9e
c1ampy <gfp16030>
2023-12-13 15:49:07 +0800

    quitable map rendering

--- maze/base.py
+++ maze/base.py
@@ -44,12 +44,13 @@ class Maze:
 				countPlain = countPlain.count('.')
 				if countPlain > 3:
 					map[2 * x][2 * y] = '.'
-				
+		
 		self.map = map
 		self.columnNumber = 2 * columnNumber + 1
 		self.rowNumber = 2 * rowNumber + 1
 		self.beginPoint = ((beginPoint // columnNumber) * 2 + 1, (beginPoint % columnNumber) * 2 + 1)
 		self.endPoint = ((endPoint // columnNumber) * 2 + 1, (endPoint % columnNumber) * 2 + 1)
+		self.curPoint = self.beginPoint
 		self.map[self.beginPoint[0]][self.beginPoint[1]] = 'S'
 		self.map[self.endPoint[0]][self.endPoint[1]] = 'T'
 			
@@ -78,9 +79,27 @@ class Maze:
 			screen.fill(i[0], i[1])
 			pygame.display.flip()
 
-		running = True
-		while running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					running = False
-		pygame.quit()
+		# running = True
+		# while running:
+		# 	for event in pygame.event.get():
+		# 		if event.type == pygame.QUIT:
+		# 			running = False
+		# pygame.quit()
+	
+	def throwHitWallException(self) -> None:
+		"""
+		report hit wall msg on visualizer
+		"""
+		print("Ouch, you've hit the wall!")
+	
+	def throwNotStoppingAtTheEndException(self) -> None:
+		"""
+		report not stoppint at the end msg on visualizer
+		"""
+		print("You didn't stop at the end.")
+
+	def move(self, direction : str) -> None:
+		if direction not in ["up", "down", "left", "right"]:
+			raise Exception("Wrong direction given to Maze.move() method.")
+	
+	def 
